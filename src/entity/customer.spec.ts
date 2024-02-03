52b9c7c3ce9fbc18a403681723ee3aaf5dbd2b47
João Pedro <jpedrodaibello1999@outlook.com>
2024-02-03 11:36:55 -0300

    test: create customer tests

--- src/entity/customer.spec.ts
+++ src/entity/customer.spec.ts
@@ -1,6 +1,49 @@
+import Address from "./address";
+import Customer from "./customer";
+
 describe("Customer unit tests", () => {
-	it("should get 1 as result", () => {
-		const result = 1;
-		expect(result).toBe(1);
+
+	it("should throw error when ID is empty", () => {
+		expect(() => new Customer("", "João")).toThrow("ID is required");
+	});
+
+	it("should throw error when name is empty", () => {
+		expect(() => new Customer("123", "")).toThrow("Name is required");
+	});
+
+	it("should change name", () => {
+		// Arrange
+		const customer = new Customer("123", "John");
+
+		// Act
+		customer.changeName("João");
+
+		// Assert
+		expect(customer.name).toBe("João");
+	});
+
+	it("should activate customer", () => {
+		const customer = new Customer("123", "Customer 1");
+		const address = new Address("Street 1", 123, "12345-678", "São Paulo");
+		customer.address = address;
+
+		customer.activate();
+
+		expect(customer.isActive()).toBe(true);
+	});
+
+	it("should throw error when address is undefined", () => {
+		expect(() => {
+			const customer = new Customer("123", "Customer 1");
+			customer.activate();
+		}).toThrow("Address is mandatory to activate a customer");
+	});
+
+	it("should dectivate customer", () => {
+		const customer = new Customer("123", "Customer 1");
+
+		customer.deactivate();
+
+		expect(customer.isActive()).toBe(false);
 	});
 });
--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,10 +1,10 @@
 import Address from "./address";
 
 export default class Customer {
-	_id: string;
-	_name: string;
-	_address!: Address;
-	_active: boolean = true;
+	private _id: string;
+	private _name: string;
+	private _address!: Address;
+	private _active: boolean = false;
 
 	constructor(id: string, name: string) {
 		this._id = id;
@@ -22,8 +22,17 @@ export default class Customer {
 		}
 	}
 
-	setName(name: string) {
+	get name(): string {
+		return this._name;
+	}
+
+	changeName(name: string) {
 		this._name = name;
+		this.validate();
+	}
+
+	isActive(): boolean {
+		return this._active;
 	}
 
 	activate() {
