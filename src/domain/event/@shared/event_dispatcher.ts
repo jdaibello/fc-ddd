8b01d07a9f44c5fe74951048f456f1304f90892b
João Pedro <jpedrodaibello1999@outlook.com>
2024-02-25 14:18:28 -0300

    feat(domain_events): implement event register

--- src/domain/event/@shared/event_dispatcher.ts
+++ src/domain/event/@shared/event_dispatcher.ts
@@ -3,19 +3,26 @@ import EventDispatcherInterface from "./event_dispatcher.interface";
 import EventHandlerInterface from "./event_handler.interface";
 
 export default class EventDispatcher implements EventDispatcherInterface {
+	private eventHandlers: { [eventName: string]: EventHandlerInterface[] } = {};
+
+	get getEventHandlers(): { [eventName: string]: EventHandlerInterface[] } {
+		return this.eventHandlers;
+	}
+
 	notify(event: EventInterface): void {
-		throw new Error("Method not implemented.");
 	}
 
 	register(eventName: string, handler: EventHandlerInterface<EventInterface>): void {
-		throw new Error("Method not implemented.");
+		if (!this.eventHandlers[eventName]) {
+			this.eventHandlers[eventName] = [];
+		}
+
+		this.eventHandlers[eventName].push(handler);
 	}
 
 	unregister(eventName: string, handler: EventHandlerInterface<EventInterface>): void {
-		throw new Error("Method not implemented.");
 	}
 
 	unregisterAll(): void {
-		throw new Error("Method not implemented.");
 	}
 }
